0f91d534d5c3a1ac18fc20c3306dc5aa5c84bcfd
Nirav Khant <29600493+niravkhant@users.noreply.github.com>
2024-02-28 01:04:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<div align="center">
+  <br />
+  <h2 align="center">OmniFood Website</h2>
+  <div align="center">
+     Develop a Omnifood website using Vite, Vanilla JavaScript, HTML, and CSS. I've utilized the power of flexbox and grid properties to ensure a seamless and visually stunning website.🌐💻
+  </div>
+  
+</div>
+
+### Demo Screeshots
+
+![OmniFood_website Desktop Demo](./assets/readme/omnifood-img.png "Desktop Demo")
+
+## 🔥 Some key features of this effort include:
+
+- ✨ Experience smooth scrolling and subtle animations
+- 📱 Enjoy a fully functional mobile navigation button
+- 💻 Compatibility across all devices - mobiles, tablets, and laptops
+- 🎨 A beautiful and user-friendly interface
+
+## 👨🏻‍💻 Technologies Used
+
+<div align="left">
+    <img src="https://skillicons.dev/icons?i=vite,html,css,javascript" /><br>
+</div>
+
+## <a name="quick-start">🤸 Quick Start</a>
+
+Follow these steps to set up the project locally on your machine.
+
+**Prerequisites**
+
+Make sure you have the following installed on your machine:
+
+<div align="left">
+    <img src="https://skillicons.dev/icons?i=git,nodejs" /><br>
+</div>
---
+++ js/script.js
@@ -0,0 +1,72 @@
+// -------- Make Mobile Navigation Work -------- //
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// -------- Smooth scrolling animation -------- //
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("header__menu-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
+// -------- Sticky Navogation -------- //
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-650px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+// ----- AOS Animation ----- //
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+AOS.init({
+  duration: 1000,
+  offset: 100,
+});
